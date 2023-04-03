1c8ec7b893d3f4ff8a72f5614b3fa3b3af5bf995
陈文发 <1151293565@qq.com>
2023-04-03 15:32:24 +0800

    fix: 打包构建

--- src/global.ts
+++ src/global.ts
@@ -1,2 +1,3 @@
-export const apiURL = import.meta.env.PROD ? 'https://kongfandong.cn' : '/api';
+export const apiURL = import.meta.env.PROD ? '/api' : '/api';
+// 'https://kongfandong.cn'
 export const publicPath = import.meta.env.VITE_APP_BUILD_MODE === 'crx' ? './' : '/Dashboard/'
